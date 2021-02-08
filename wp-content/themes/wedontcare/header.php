8b0d94a74fdbfa5d8f286fee0cc9df9808b504ae
vikputthiscodeongit <vikputthiscodeongit@users.noreply.github.com>
2021-02-08 22:16:20 +0100

    Change file paths

--- wp-content/themes/wedontcare/header.php
+++ wp-content/themes/wedontcare/header.php
@@ -17,11 +17,11 @@
     <?php wp_head(); ?>
 
     <!-- Favicon stuff -->
-    <link rel="apple-touch-icon" sizes="180x180" href="dist/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="dist/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="dist/favicon/favicon-16x16.png">
-    <link rel="manifest" href="dist/favicon/site.webmanifest">
-    <link rel="mask-icon" href="dist/favicon/safari-pinned-tab.svg" color="#000000">
+    <link rel="apple-touch-icon" sizes="180x180" href="/dist/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/dist/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/dist/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/dist/favicon/site.webmanifest">
+    <link rel="mask-icon" href="/dist/favicon/safari-pinned-tab.svg" color="#000000">
     <meta name="msapplication-TileColor" content="#000000">
     <meta name="theme-color" content="#000000">
   </head>
